0d6c09c9776c1940dd0f4363701af8cdf90097c6
Anna <slobodian.ann@gmail.com>
2020-02-04 16:53:25 +0200

    add task2

---
+++ task2/task2.js
@@ -0,0 +1,22 @@
+/*for (let i = 1; i < 8; i++){
+    if (i==6){
+        continue;
+    }
+    console.log(i);
+}*/
+/*let i = 0;
+while(i < 8){
+    i++;
+    if (i==6){
+        continue;
+    }
+    console.log(i);
+}*/
+let i = 0;
+do {
+    i++;
+    if (i==6){
+        continue;
+    }
+    console.log(i);
+} while(i<8)
